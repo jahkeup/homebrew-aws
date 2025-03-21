38bd014bb60f13601cbc11c05a92be096047b9b1
Jacob Vallejo <jakeev@amazon.com>
2025-03-20 18:23:27 -0700

    airesntiarsntiarstnast ssm

--- Casks/amazon-ssm-agent.rb
+++ Casks/amazon-ssm-agent.rb
@@ -19,15 +19,23 @@ cask "amazon-ssm-agent" do
   build_version = "3.3.1957.0-1"
   version build_version.split("-").first
 
-  pkg_file = "amazon-ssm-agent-#{build_version}_#{arch}.pkg"
-  url "https://aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ssm-agent/#{pkg_file}",
-      verified: "aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ssm-agent/"
-  if Hardware::CPU.intel?
-        arch = "x86_64"
-        sha256 "1a73cedc2823ee406d1235ec0f06abee39ded85fc0bbf25ed482fa77d42c3eb3"
-  else
-        arch = "arm64"
-        sha256 "f7008138067da831d6ca3c05d464c060c7c6000ddc505770e1ecf43e34c8a254"
+  on_intel do
+    pkg_file = "amazon-ssm-agent-#{build_version}_x86_64.pkg"
+
+    url "https://aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ssm-agent/#{pkg_file}",
+        verified: "aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ssm-agent/"
+    sha256 "1a73cedc2823ee406d1235ec0f06abee39ded85fc0bbf25ed482fa77d42c3eb3"
+
+    pkg pkg_file
+  end
+  on_arm do
+    pkg_file = "amazon-ssm-agent-#{build_version}_arm64.pkg"
+
+    url "https://aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ssm-agent/#{pkg_file}",
+        verified: "aws-homebrew.s3-us-west-2.amazonaws.com/cask/amazon-ssm-agent/"
+    sha256 "f7008138067da831d6ca3c05d464c060c7c6000ddc505770e1ecf43e34c8a254"
+
+    pkg pkg_file
   end
 
   script = Tempfile.new('ensure_no_com.amazon.aws.ssm_')
@@ -69,8 +77,6 @@ cask "amazon-ssm-agent" do
   uninstall_preflight do
     system_command "/bin/bash", args: [script.path], sudo: true
   end
-
-  pkg pkg_file
 
   livecheck do
     url "https://github.com/aws/amazon-ssm-agent.git"
